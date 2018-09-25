ec3c233bbf1acd9c24cebc0b066a17c388198bcc
fullyalive <wogud1202@gmail.com>
2018-09-25 23:35:54 +0900

    Defining Mutations

--- graphql/db.js
+++ graphql/db.js
@@ -1,43 +1,50 @@
-export const people = [
+let movies = [
   {
-    id: "0",
-    name: "fullyalive",
-    age: 28,
-    gender: "male"
+    id: 0,
+    name: "Star Wars - The new one",
+    score: 1
   },
   {
-    id: "1",
-    name: "alwaysawake",
-    age: 23,
-    gender: "female"
+    id: 1,
+    name: "Avengers - The new one",
+    score: 8
   },
   {
-    id: "2",
-    name: "user1",
-    age: 28,
-    gender: "male"
+    id: 2,
+    name: "The Godfather I",
+    score: 99
   },
   {
-    id: "3",
-    name: "user2",
-    age: 28,
-    gender: "male"
-  },
-  {
-    id: "4",
-    name: "user3",
-    age: 28,
-    gender: "male"
-  },
-  {
-    id: "5",
-    name: "user4",
-    age: 28,
-    gender: "male"
+    id: 3,
+    name: "Logan",
+    score: 2
   }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === String(id));
-  return filteredPeople[0];
+  const filteredMovies = movies.filter(movie => movie.id === String(id));
+  return filteredMovies[0];
+};
+
+export const deleteMovie = id => {
+  // 같은 id를 가지지 못한 Movie의 배열
+  const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
